854a2d21f9257f6c608c403ea6dab660c6aa0168
Nhat Nguyen <nhatndm1193@gmail.com>
2021-03-06 00:37:29 +0800

    added guard

---
+++ src/shared/guard/index.admin.ts
@@ -0,0 +1,49 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+
+// CONSTANT
+import { ERROR_MESSAGE_CODE } from '@/constant';
+
+// FIREBASE
+import * as admin from 'firebase-admin';
+
+@Injectable()
+export class AuthAdminTokenGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { headers } = request;
+    const token = headers['authorization'];
+
+    try {
+      const decode = await admin.auth().verifyIdToken(token);
+      const uid = decode.uid;
+
+      const checkAdmin = await (
+        await admin
+          .firestore()
+          .collection('admin')
+          .where('uid', '==', uid)
+          .get()
+      ).docs[0].data();
+
+      if (!checkAdmin) {
+        throw new Error();
+      }
+
+      return true;
+    } catch (error) {
+      throw new HttpException(
+        {
+          code: HttpStatus.UNAUTHORIZED,
+          message: ERROR_MESSAGE_CODE.error_002,
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+  }
+}
---
+++ src/shared/guard/index.ts
@@ -0,0 +1,35 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+
+// CONSTANT
+import { ERROR_MESSAGE_CODE } from '@/constant';
+
+// FIREBASE
+import * as admin from 'firebase-admin';
+
+@Injectable()
+export class AuthTokenGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { headers } = request;
+    const token = headers['authorization'];
+
+    try {
+      await admin.auth().verifyIdToken(token);
+      return true;
+    } catch (error) {
+      throw new HttpException(
+        {
+          code: HttpStatus.UNAUTHORIZED,
+          message: ERROR_MESSAGE_CODE.error_002,
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+  }
+}
